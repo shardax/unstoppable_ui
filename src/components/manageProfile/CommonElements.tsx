f8522d0ab29fcfef72c1b465a18d04f643e25b44
shardax <saradarails@gmail.com>
2020-06-24 12:49:15 -0400

    Use FlexView attributes instead of style

--- src/components/manageProfile/CommonElements.tsx
+++ src/components/manageProfile/CommonElements.tsx
@@ -8,16 +8,18 @@ const PrintUserInfo: React.FC = ({  }) => {
   const store = useDataStore();
   return (
     <div>
-      <FlexView style={{ width: 30, height: 70}}></FlexView>
-      <FlexView  style={{ width: 250, height: 20 }}>
-        <FlexView  style={{ width: 20, height: 10 }}></FlexView>
-        <FlexView  style={{ width: 230, height: 10 }}>
+      <FlexView basis={30} height={70}>
+      </FlexView>
+      <FlexView  basis={250} height={20}>
+        <FlexView  basis={20} height={10}>
+        </FlexView>
+        <FlexView  basis={230} height={10}>
           <b>{store.username}</b>, {store.profile.age}</FlexView>
         </FlexView >
-          <FlexView  style={{ width: 250, height: 30 }}>
-            <FlexView  style={{ width: 20, height: 30 }}></FlexView>
-            <FlexView  style={{ width: 230, height: 30 }}>
-                {store.profile.city}, {store.profile.state} {store.profile.zipcode}
+          <FlexView  basis={250} height={30}>
+            <FlexView  basis={20} height={30}></FlexView>
+            <FlexView  basis={230} height={30}>
+              {store.profile.city}, {store.profile.state} {store.profile.zipcode}
           </FlexView>
       </FlexView >
     </div>
