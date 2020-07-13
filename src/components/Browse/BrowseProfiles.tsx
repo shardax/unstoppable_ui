793548fb6ae78727a809538fa7f813c4bd8ea845
Zev Stravitz <zevstravitz@verizon.net>
2020-07-13 17:45:25 -0400

    new layout

--- src/components/Browse/BrowseProfiles.tsx
+++ src/components/Browse/BrowseProfiles.tsx
@@ -73,9 +73,12 @@ import { useObserver } from "mobx-react";
   return useObserver(() => (
     <>
       <div>
-        <div className="browse-filter-row"> 
-          <input className="mdc-elevation--z1 browse-search global-input" value={filter} onChange={e => setFilter(e.target.value)} placeholder="Search by Cancer Type OR Zipcode OR City" />
-            <RangeSlider ageRange={ageRange} onChange={handleChange}/>
+          <div className="browse-sticky-nav">
+            <h3>Browse Profiles</h3>
+            <div className="browse-filter-row"> 
+              <input className="mdc-elevation--z1 browse-search global-input" value={filter} onChange={e => setFilter(e.target.value)} placeholder="Search by Cancer Type OR Zipcode OR City" />
+                <RangeSlider ageRange={ageRange} onChange={handleChange}/>
+              </div>
           </div>
           <div className="profile-browse-grid">
             {userCollection.map((profile: any) => (
