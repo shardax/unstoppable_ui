6c0a7a019aa5cf6de2259a7bedd51967bac98690
Zev Stravitz <zevstravitz@verizon.net>
2020-07-17 12:53:49 -0500

    updating detailed user page

--- src/components/Users/UserSection.tsx
+++ src/components/Users/UserSection.tsx
@@ -1,7 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 import { ALLPROFILESURL, ROOTURL, PROFILEURL } from "../../constants/matcher";
-import './index.scss'
+import './index.scss';
+
+import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
+import NotesIcon from '@material-ui/icons/Notes';
+import SportsTennisIcon from '@material-ui/icons/SportsTennis';
 
 const UserSection: React.FC<{id: string}> = ({ id }) => {
 
@@ -24,6 +28,15 @@ const UserSection: React.FC<{id: string}> = ({ id }) => {
     getProfile();
   }, [])
 
+  const ProfileIconRow = ({ icon, field, answer}) => {
+    return (
+      <div className="full-profile-icon-row">
+        {icon}
+        <span className="muted-text">{field}: {answer} </span>
+      </div>
+    )
+  }
+
   if (!user || loading ) {
     return <div>Loading...</div>
   }
@@ -31,11 +44,22 @@ const UserSection: React.FC<{id: string}> = ({ id }) => {
   return (
     <div className="user-section-wrapper">
       <div className="user-metadata">
-        <h1>{user.name}</h1>
         <img className="user-section-image" src={ROOTURL + user.photo} />
       </div>
-      <div className="user-section-qa">
-        Test
+      <div className="user-section-data">
+        <h1 style={{ fontSize: "26px" }}>{user.name}  · <span className="full-profile-location muted-text">city, state</span></h1>
+        <p className="muted-text">{user.age} years old, {user.cancer_location} cancer</p>
+
+        <div className="full-profile-icon-row">
+          <NotesIcon className="full-profile-icon" />
+          <div className="full-profile-das">
+            <p>{user.details_about_self}</p>
+          </div>
+        </div>
+
+        <ProfileIconRow field={"Fitness Level"} answer={user.fitness_level} icon={<FitnessCenterIcon className="full-profile-icon" />}/>
+        <ProfileIconRow field={"Activities"} answer={"test"} icon={<SportsTennisIcon className={"full-profile-icon"} />} />
+        <p>Not done! More to come down here...</p>
       </div>
     </div>
   )
