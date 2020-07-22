aaf1964466f5b0939c31559e189e7e54c635eb3d
csheng05 <csheng05@gmail.com>
2020-07-22 15:59:42 -0400

    register update

--- src/pages/register.js
+++ src/pages/register.js
@@ -2,7 +2,7 @@ import React from 'react'
 /*import Register from '../components/Register/Register'*/
 import Register2 from '../components/Register/Register2'
 
-export default function Login() {
+export default function Register() {
   return (
   <div>
     <Register2 />
