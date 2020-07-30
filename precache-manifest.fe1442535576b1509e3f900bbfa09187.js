2f3682b47a78fda849c13c15056d865ccf10dab0
RohithChintala <roffsicle@gmail.com>
2020-07-30 12:41:54 -0400

    Updates

---
+++ precache-manifest.fe1442535576b1509e3f900bbfa09187.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab5076da6554643e3683eda4f0e9d0f",
+    "url": "/unstoppable_ui/index.html"
+  },
+  {
+    "revision": "29bfe23f7b8f3a832633",
+    "url": "/unstoppable_ui/static/css/2.6ef4c1a0.chunk.css"
+  },
+  {
+    "revision": "2d287c8a8bf16dc2a252",
+    "url": "/unstoppable_ui/static/css/main.99a7a1b1.chunk.css"
+  },
+  {
+    "revision": "29bfe23f7b8f3a832633",
+    "url": "/unstoppable_ui/static/js/2.795899bf.chunk.js"
+  },
+  {
+    "revision": "b85e7adfa87415f6f79cfa78116c6551",
+    "url": "/unstoppable_ui/static/js/2.795899bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d287c8a8bf16dc2a252",
+    "url": "/unstoppable_ui/static/js/main.ad6456de.chunk.js"
+  },
+  {
+    "revision": "5a2a063c52c01a0c58dc",
+    "url": "/unstoppable_ui/static/js/runtime-main.b3ab1ddb.js"
+  },
+  {
+    "revision": "6b411738823deab41a8ed25b14888fca",
+    "url": "/unstoppable_ui/static/media/2Unstoppable_logo.6b411738.png"
+  },
+  {
+    "revision": "77d71a9aa5eb6cc9d15f8d67fcede327",
+    "url": "/unstoppable_ui/static/media/2unstoppable.77d71a9a.png"
+  },
+  {
+    "revision": "96f1c901c087fb64019f7665f7f8aca6",
+    "url": "/unstoppable_ui/static/media/ionicons.96f1c901.woff2"
+  },
+  {
+    "revision": "99b863497156d4478ec3489fefb3815c",
+    "url": "/unstoppable_ui/static/media/ionicons.99b86349.woff"
+  }
+]);
