b46d00270cee150c2d209c180eaf4f132ec85140
shardax <sarada.chintala@gmail.com>
2020-07-31 11:24:58 -0400

    change Root url to https

--- src/constants/matcher.js
+++ src/constants/matcher.js
@@ -1,5 +1,5 @@
 //export const ROOTURL = "http://localhost:3001"
-export const ROOTURL = "http://uns1.herokuapp.com"
+export const ROOTURL = "https://uns1.herokuapp.com"
 export const LOGINURL = ROOTURL + "/users/sign_in"
 export const LOGOUTURL = ROOTURL + "/users/sign_out"
 export const ALLPROFILESURL = ROOTURL + "/profiles.json"
