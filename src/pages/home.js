b3d12bbaa8a9fbcc3b7273a863c229b9e0de50a5
shardax <sarada.chintala@gmail.com>
2020-08-01 22:33:29 -0400

    Minor fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,11 +7,10 @@ import SignIn2 from '../components/LogIn/SignIn2'
 export default function Home() {
   const storedData = localStorage.getItem("userStore");
   const store = useDataStore();
-
   if (!storedData) {
     return (<SignIn2 />)
   } else {
-    if (!store.isLoggedIn) {
+    if (store && !store.isLoggedIn) {
       return (<SignIn2 />)
     } else {
       return (
@@ -21,3 +20,4 @@ export default function Home() {
       )
     }
   }
+}
