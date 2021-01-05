4374dc62d631c55f179b7037a12bad79f0dcdd13
shardax <shardax2000@gmail.com>
2021-01-04 19:47:41 -0500

    Add number of unread messages as badge in chatroom

--- src/components/Chatroom/ChatroomListItem.jsx
+++ src/components/Chatroom/ChatroomListItem.jsx
@@ -2,14 +2,15 @@ import React, {useEffect, useContext} from 'react'
 import { Link } from "react-router-dom";
 import { ActionCableContext, useDataStore } from "../../UserContext";
 import './index.scss';
-
+import Badge from '@material-ui/core/Badge';
+import { createBrowserHistory } from 'history'
 
 const ChatroomListItem = ({chatroom, viewOnly}) => {
   const store = useDataStore();
   const cable = useContext(ActionCableContext);
+  const history = createBrowserHistory({ forceRefresh: true });
   
   useEffect ( () => {
-    //console.log("mojo", store.chatroomsInitialize);
     if (!store.chatroomsInitialize){
       const channel =  cable.subscriptions.create( 
                         { channel: "ChatroomMessagesChannel", id: chatroom.id },
@@ -31,19 +32,29 @@ const ChatroomListItem = ({chatroom, viewOnly}) => {
     }
   } , [chatroom])
   
-  
+  const handleClick = (event) => {
+    console.log(event.target.value);
+    history.push("/chatroomDetails/" + event.target.value);
+  }
+
 
  return (
-  <div>
+  <div>{/**chatrooms && chatrooms.map(chatroom => (
+   *<Badge color="primary" badgeContent={chatroom.number_of_unreads}><button type="button" value={chatroom.id} onClick={(e) => handleClick(e)}>{chatroom.name}</button></Badge>))
+   * */}
+
     <Link to={`chatroomDetails/${chatroom.id}`}>
     
         <div className={"single-conversation-wrapper "}>
-            <div className="conversation-subject">
-              {chatroom.name}
+        <div className="conversation-subject">
+              {chatroom.name}<Badge color="primary" badgeContent={chatroom.number_of_unreads}></Badge>
             </div>
           <hr></hr>
         </div>
     </Link>
+    {/** <div className="conversation-subject">
+      <Badge color="primary" badgeContent={chatroom.number_of_unreads}><button type="button" value={chatroom.id} onClick={(e) => handleClick(e)}>{chatroom.name}</button></Badge>
+  </div> **/}
   </div>
  )
 }
