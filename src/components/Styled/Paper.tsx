9c0a0109b8e338d5e4801c1486ed252a7514b116
Sean <seanha2013@gmail.com>
2021-11-06 17:36:45 -0400

    Remove changes from profile, added to settings

--- src/components/Styled/Paper.tsx
+++ src/components/Styled/Paper.tsx
@@ -21,12 +21,6 @@ const Paper = styled.div<PaperProps>`
   border: ${props => props.border ? props.border : "none"};
   margin: ${props => props.margin ? props.margin : "2em 0em"};
   box-shadow: 0 2px 4px -1px rgba(0,0,0,.2),0 4px 5px 0 rgba(0,0,0,.14),0 1px 10px 0 rgba(0,0,0,.12) !important;
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  .profile-block {
-    width: 48%;
-  };
   `
 
 export default Paper;
