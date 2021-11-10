075f6e518614fa5ca8c3e6eaca9012b83b966b00
Rae <rachelw@terpmail.umd.edu>
2021-11-10 17:51:00 -0500

    added accordian, created buttons and other fields needed

--- src/components/Browse/ProfileForm.tsx
+++ src/components/Browse/ProfileForm.tsx
@@ -15,16 +15,7 @@ import './Browse.scss'
 import { useObserver } from "mobx-react";
 import Button from '../Styled/Button';
 import Select from '../Styled/Select';
-import colors from "../../assets/colors";
-
-// accordian imports 
-import Accordion from '@material-ui/core/Accordion';
-import { AccordionSummary } from "@material-ui/core";
-import { AccordionDetails } from "@material-ui/core";
-import { Typography } from "antd";
-import { BsChevronBarExpand } from "react-icons/bs";
-
-// chat imports 
+import colors from "../../assets/colors"
 import ChatIcon from '@material-ui/icons/Chat';
 import SortBarDisplay from './SortBarDisplay'
 import SortIcon from '@material-ui/icons/Sort';
@@ -281,25 +272,37 @@ import TimeAgo from 'timeago-react';
   }
 
   return useObserver(() => (
-    // consider: using a component to represent each search widget 
     <>
-      <div className="browse-container">
-          <h3 className="pageHeader">Browse Profiles</h3>
-          <p>Enter keywords separated by spaces in search box(for e.g: TNBC DCIS Stage)</p>
+      <div>
           <div className="browse-sticky-nav">
-            <h5 className="boldedSubheader">I'm looking for an exercise buddy:</h5>
-            
-            {/* age slider */}
-            <div className="range-slider search-widget">
+            <h3>Browse Profiles</h3>
+            <p>Enter keywords separated by spaces in search box(for e.g: TNBC DCIS Stage)</p>
+            <div className="browse-filter-row"> 
+              <Tooltip title="Add any word including the cancer type, state, zipcode or city. Example: 1) 20854 Breast Ovarian 2)  VA TNBC 3)   Lung Rockville Gaithersburg 4)   MD DCIS kidney Stage 3">
+                <input className="browse-search global-input" value={filter} onChange={e => setFilter(e.target.value)} placeholder="Free Text Search" />
+              </Tooltip>
+            <div>
+              <Tooltip title="Contains any of the keywords">
+                <label>
+                  <Radio value="OR" color="primary" checked={keywordSearchType==="OR"} onChange={(e) => handleRadioSearch(e)}  />OR
+                </label>
+              </Tooltip>
+              <Tooltip title="Contains all of the keywords">
+                <label>
+                  <Radio value="AND" color="primary" checked={keywordSearchType === "AND"}  onChange={(e) => handleRadioSearch(e)}  />AND
+                </label>
+              </Tooltip >
+            </div>
+            <div className="range-slider">
               <RangeSlider ageRange={ageRange} onChange={handleChange}/>
             </div>
-            {/* distance */}
-            <div className="range-slider search-widget">
-            {!reset && <DiscreteSlider  distance={distance} onChange={handleDistanceChange}/>}
-            {reset && <DiscreteSlider  distance={DISTANCE_WITHIN_CONSTANT} onChange={handleDistanceChange}/>}
-            </div>
-            {/* state */}
-            {(store.uniqueLists && store.uniqueLists.unique_state_codes.length > 1) && <div className="search-widget">
+            <Select onChange={e => setCancerTypeKeyword(e.target.value)} margin="0em 2em" value={cancerTypeKeyword}>
+            <option className="selector" value="" label="- Select cancer type -" />
+            {CANCERLOCATIONLIST.map((cancerLoc: any) => (
+              <option className="selector" value={cancerLoc} label={cancerLoc} />
+            ))}
+            </Select> 
+            {(store.uniqueLists && store.uniqueLists.unique_state_codes.length > 1) && <div>
               <Select onChange={e => setStateCodeKeyword(e.target.value)} margin="0em 2em" value={stateCodeKeyword}>
                 <option className="selector" value="" label="- Select State -" />
                 {store.uniqueLists.unique_state_codes.map((sc: any) => (
@@ -307,8 +310,15 @@ import TimeAgo from 'timeago-react';
               ))}
               </Select>
             </div>}
-            {/* city */}
-            {(store.uniqueLists && store.uniqueLists.unique_cities.length > 1) && <div className="search-widget">
+            {(store.uniqueLists && store.uniqueLists.unique_zipcodes.length > 1) && <div>
+              <Select onChange={e => setZipcodeKeyword(e.target.value)} margin="0em 2em" value={zipcodeKeyword}>
+                <option className="selector" value="" label="- Select Zipcode -" />
+                {store.uniqueLists.unique_zipcodes.map((z: any) => (
+                <option className="selector" value={z} label={z} />
+              ))}
+              </Select>
+            </div>}
+            {(store.uniqueLists && store.uniqueLists.unique_cities.length > 1) && <div>
               <Select onChange={e => setCityKeyword(e.target.value)} margin="0em 2em" value={cityKeyword} >
                 <option className="selector" value="" label="- Select City -" />
                 {store.uniqueLists.unique_cities.map((c: any) => (
@@ -316,8 +326,11 @@ import TimeAgo from 'timeago-react';
               ))}
               </Select>
             </div>}
-            {/* active users filter */}
-            <div className="range-slider search-widget">
+            <div className="range-slider">
+            {!reset && <DiscreteSlider  distance={distance} onChange={handleDistanceChange}/>}
+            {reset && <DiscreteSlider  distance={DISTANCE_WITHIN_CONSTANT} onChange={handleDistanceChange}/>}
+            </div>
+            <div className="range-slider">
               <Tooltip title="Displays Users active since the last 5 minutes">
                 <FormGroup row>
                 <FormControlLabel
@@ -334,117 +347,23 @@ import TimeAgo from 'timeago-react';
                 </FormGroup>
               </Tooltip >
               </div>
-              {/* yes to long distance button */}
-              <div className="range-slider search-widget">
-              <Tooltip title="Displays Users active since the last 5 minutes">
-                <FormGroup row>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={activeUsers}
-                      onChange={e => handleActiveUsers(e)}
-                      name="activeUsers"
-                      color="primary"
-                    />
-                  }
-                  label="Yes to a Long-distance Buddy"
-                />
-                </FormGroup>
-              </Tooltip >
-              </div>
-
-            {/* <h5 className="boldedSubheader">Advanced Search</h5> */}
-            
-            {/* free text search */}
-            <div className="browse-filter-row"> 
-            <Accordion className="no-border-accordian"> 
-            <AccordionSummary
-              expandIcon={<BsChevronBarExpand/>}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography className="boldedSubheader">Advanced Search</Typography>
-          </AccordionSummary>
-
-          <AccordionDetails>
-              <Tooltip title="Add any word including the cancer type, state, zipcode or city. Example: 1) 20854 Breast Ovarian 2)  VA TNBC 3)   Lung Rockville Gaithersburg 4)   MD DCIS kidney Stage 3">
-                <input className="browse-search global-input" value={filter} onChange={e => setFilter(e.target.value)} placeholder="Free Text Search" />
-              </Tooltip>
-            {/* cancer type*/}
-            <Select onChange={e => setCancerTypeKeyword(e.target.value)} margin="0em 2em" value={cancerTypeKeyword}>
-            <option className="selector" value="" label="- Select cancer type -" />
-            {CANCERLOCATIONLIST.map((cancerLoc: any) => (
-              <option className="selector" value={cancerLoc} label={cancerLoc} />
-            ))}
-            </Select> 
-            {/* zipcode */} 
-            {(store.uniqueLists && store.uniqueLists.unique_zipcodes.length > 1) && <div>
-              
-              <Select onChange={e => setZipcodeKeyword(e.target.value)} margin="0em 2em" value={zipcodeKeyword}>
-                <option className="selector" value="" label="- Select Zipcode -" />
-                {store.uniqueLists.unique_zipcodes.map((z: any) => (
-                <option className="selector" value={z} label={z} />
-              ))}
-              </Select>
-            </div>}
-            {/* Which of the following best describes you? */} 
-            {(store.uniqueLists && store.uniqueLists.unique_zipcodes.length > 1) && <div>
-              
-              <Select onChange={e => setZipcodeKeyword(e.target.value)} margin="0em 2em" value={zipcodeKeyword}>
-                <option className="selector" value="" label="- Which of the following best describes you? -" />
-                {store.uniqueLists.unique_zipcodes.map((z: any) => (
-                <option className="selector" value={z} label={z} />
-              ))}
-              </Select>
-            </div>}
-            {/* Favorite activities */} 
-            {(store.uniqueLists && store.uniqueLists.unique_zipcodes.length > 1) && <div>
-              
-              <Select onChange={e => setZipcodeKeyword(e.target.value)} margin="0em 2em" value={zipcodeKeyword}>
-                <option className="selector" value="" label="- Favorite activities -" />
-                {store.uniqueLists.unique_zipcodes.map((z: any) => (
-                <option className="selector" value={z} label={z} />
-              ))}
-              </Select>
-            </div>}
-            {/* Preferred exercise location */} 
-            {(store.uniqueLists && store.uniqueLists.unique_zipcodes.length > 1) && <div>
-              
-              <Select onChange={e => setZipcodeKeyword(e.target.value)} margin="0em 2em" value={zipcodeKeyword}>
-                <option className="selector" value="" label="- Preferred exercise location -" />
-                {store.uniqueLists.unique_zipcodes.map((z: any) => (
-                <option className="selector" value={z} label={z} />
-              ))}
-              </Select>
-            </div>}
-
-              {/* <div className="range-slider">
-                <Tooltip title="Sort Users">
+              <div className="range-slider">
+                {/*<Tooltip title="Sort Users">
                   <SortIcon />
-                </Tooltip > 
+                </Tooltip > */}
                 {!reset && <SortBarDisplay onChange={handleOrderChange} distanceOrder={distanceOrder} ageOrder={ageOrder} lastOnineOrder={lastOnlineOrder} newestMemberOrder={newestMemberOrder} resetFunction={handleResetCompletion} reset={reset} />}
                 {reset && <SortBarDisplay onChange={handleOrderChange} distanceOrder={"asc"} resetFunction={handleResetCompletion} reset={reset} />}
-              </div> */}
-
-              </AccordionDetails>
-              </Accordion>
-
-              {/* search button */}
-              <div className="range-slider search-widget">
-                  <Button id="prev" margin="2em 1.5em" padding="10px 20px" background="#ffe7ed"  color="#f0658c" onClick={(e)=>{handleClearSelections()}}>
-                      SEARCH
-                  </Button>
               </div>
-              {/* reset button */}
-              <div className="range-slider search-widget">
-                  <Button id="prev" margin="2em 1.5em" padding="10px 20px" background="#ffe7ed" color="#f0658c" onClick={(e)=>{handleClearSelections()}}>
-                      RESET
-                  </Button>
+              <div className="range-slider">
+                  <Button id="prev" margin="2em 1.5em" padding="10px 20px"
+                                            onClick={(e)=>{handleClearSelections()}}>
+                                            Reset all selections
+                                        </Button>
               </div>
             </div>
           </div>
           <div className="range-slider">
-            <h4 className="totalUserProfileHeader"><b> Total User Profile - {numberOfProfiles}</b></h4><h6>{searchTextDisplay}</h6>
+            <h4><b> {numberOfProfiles} {activeUsers ? "Active " : " "}User{(numberOfProfiles != 1) ? "s":""}</b></h4><h6>{searchTextDisplay}</h6>
           </div>
           <div className="profile-browse-grid">
             {userCollection.map((profile: any) => (
@@ -460,9 +379,7 @@ import TimeAgo from 'timeago-react';
                 onChange={handlePageChange}
               />
         </div>
-        
       </div>
-      
     </>
   ))
 }
