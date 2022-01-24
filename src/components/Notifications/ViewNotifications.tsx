93c6809a9040cc4b4ac03b58e81583d0533947e8
Sean <seanha2013@gmail.com>
2022-01-24 17:24:46 -0500

    Clear and sort notifications functionality

--- src/components/Notifications/ViewNotifications.tsx
+++ src/components/Notifications/ViewNotifications.tsx
@@ -12,22 +12,7 @@ import NotificationItem from "./NotificationItem";
 
 const ViewNotifications: React.FC = ({}) => {
   const store = useDataStore();
-  const [currentProfile, setCurrentProfile] = useState(store.profile);
-  const [dataLoading, setDataLoading] = useState("");
-  const [profileImg, setProfileImg] = useState(ROOTURL + store.avatarPath);
-  const history = useHistory();
-  const [editMode, setEditMode] = useState(false);
-
-  // Oldest at top
-  const sortAsc = function(a, b) {
-    return a.date.getTime() - b.date.getTime()
-  };
-  // Most recent at top
-  const sortDesc = function(a, b) {
-    return b.date.getTime() - a.date.getTime()
-  };
-
-  const exampleNotifications = [
+  const [notificationList, setNotificationList] = useState([
     {
       image:"/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBZEk9IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--0d5009055e89d71c1189aa1f90bf9ad5fd2c2ddf/DSC_0034.JPG",
       header:"Finish Setting Up Your Profile:",
@@ -40,7 +25,42 @@ const ViewNotifications: React.FC = ({}) => {
       description:"Hi! I just noticed we have similar cancer diagnoses, I would love to connect with you!",
       date: new Date("October 21, 2021")
     }
+  ]);
+  const sortModes = [
+    "Most Recent",
+    "Oldest",
   ]
+  const [sortMode, setSortMode] = useState(sortModes[0]);
+
+  // Oldest at top
+  const sortAsc = function(a, b) {
+    return a.date.getTime() - b.date.getTime()
+  };
+  // Most recent at top
+  const sortDesc = function(a, b) {
+    return b.date.getTime() - a.date.getTime()
+  };
+
+  // Maps a sortMode to a sortFunc
+  const getSortFunc = function(mode) {
+    switch (mode) {
+      case sortModes[0]:
+        return sortDesc;
+      case sortModes[1]:
+        return sortAsc;
+    }
+  }
+  const [sortFunc, setSortFunc] = useState(() => getSortFunc(sortMode))
+
+  // When the user clicks a dropdown item: re-sorts the notification list based on chosen item
+  const dropdownOnClick = function(mode) {
+    setSortMode(mode);
+    setSortFunc(() => getSortFunc(mode));
+  };
+
+  const clearNotifications = function() {
+    setNotificationList([]);
+  };
 
   return (
     <div>
@@ -49,18 +69,22 @@ const ViewNotifications: React.FC = ({}) => {
         <h4>Sort By:</h4>
       </div>
       <div className="interactables">
-        <DropdownButton id="sort-by" title="Most Recent">
-          {/*<Dropdown.Item onClick={}>Oldest</Dropdown.Item>*/}
-          <Dropdown.Item>Oldest</Dropdown.Item>
+        <DropdownButton id="sort-by" title={sortMode}>
+          {sortModes.map((mode) => {
+            if (mode != sortMode) {
+              return <Dropdown.Item onClick={() => dropdownOnClick(mode)}>{mode}</Dropdown.Item>
+            }
+          })}
         </DropdownButton>
 
-        <Button className="clear-all-btn">CLEAR ALL</Button>
+        {notificationList.length ? <Button className="clear-btn" onClick={()=>clearNotifications()}>CLEAR ALL</Button> :
+        <Button className="clear-btn" disabled>CLEARED</Button>}
       </div>
 
       <hr className="horizontal-line"/>
 
       <div className="notification-list">
-        {exampleNotifications.length ? exampleNotifications.sort(sortDesc).map((notification: any) => (
+        {notificationList.length ? notificationList.sort(sortFunc).map((notification: any) => (
           <NotificationItem notification={notification} />
         )) : <p className="no-notification-message">You have no new notifications at this time</p>}
       </div>
